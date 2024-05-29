f9a1bffd7d11c06bbec6cfce9ca163b34caba4c7
Linn Thit <lth1212001@gmail.com>
2024-05-29 16:50:59 +0630

    -

--- Order_System/Models/User/UserModel.cs
+++ Order_System/Models/User/UserModel.cs
@@ -1,14 +1,13 @@
-﻿namespace Order_System.Models.User
+﻿namespace Order_System.Models.User;
+
+public class UserModel
 {
-    public class UserModel
-    {
-        public long UserId { get; set; }
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string PhoneNo { get; set; } = null!;
-        public string Password { get; set; } = null!;
-        public string UserRole { get; set; } = null!;
-        public bool IsActive { get; set; }
-    }
+    public long UserId { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNo { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
+    public bool IsActive { get; set; }
 }
