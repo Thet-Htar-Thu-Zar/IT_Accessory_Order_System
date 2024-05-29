67f146721711472a7fb34efc6cb1ea3d273e5042
Linn Thit <lth1212001@gmail.com>
2024-05-29 16:52:26 +0630

    -

--- Order_System/Controllers/UserController.cs
+++ Order_System/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Order_System.Service;
 using System.Data.SqlClient;
 using System.Data;
--- Order_System/Service/AdoDotNetService.cs
+++ Order_System/Service/AdoDotNetService.cs
@@ -2,71 +2,70 @@
 using System.Data;
 using Newtonsoft.Json;
 
-namespace Order_System.Service
+namespace Order_System.Service;
+
+public class AdoDotNetService
 {
-    public class AdoDotNetService
+
+    private readonly IConfiguration _configuration;
+
+    public AdoDotNetService(IConfiguration configuration)
     {
-
-        private readonly IConfiguration _configuration;
-
-        public AdoDotNetService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        #region Query
-
-        public List<T> Query<T>(string query, SqlParameter[]? parameters = null)
-        {
-            SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
-            conn.Open();
-            SqlCommand cmd = new(query, conn);
-            cmd.Parameters.AddRange(parameters);
-            SqlDataAdapter adapter = new(cmd);
-            DataTable dt = new();
-            adapter.Fill(dt);
-            conn.Close();
-
-            string jsonStr = JsonConvert.SerializeObject(dt);
-            List<T> lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
-
-            return lst;
-        }
-
-        #endregion
-
-        #region Query First Or Default
-
-        public DataTable QueryFirstOrDefault(string query, SqlParameter[]? parameters)
-        {
-            SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
-            conn.Open();
-            SqlCommand cmd = new(query, conn);
-            cmd.Parameters.AddRange(parameters);
-            SqlDataAdapter adapter = new(cmd);
-            DataTable dt = new();
-            adapter.Fill(dt);
-            conn.Close();
-
-            return dt;
-        }
-
-        #endregion
-
-        #region Execute
-
-        public int Execute(string query, SqlParameter[]? parameters = null)
-        {
-            SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
-            conn.Open();
-            SqlCommand cmd = new(query, conn);
-            cmd.Parameters.AddRange(parameters);
-            int result = cmd.ExecuteNonQuery();
-            conn.Close();
-
-            return result;
-        }
-
-        #endregion
+        _configuration = configuration;
     }
+
+    #region Query
+
+    public List<T> Query<T>(string query, SqlParameter[]? parameters = null)
+    {
+        SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
+        conn.Open();
+        SqlCommand cmd = new(query, conn);
+        cmd.Parameters.AddRange(parameters);
+        SqlDataAdapter adapter = new(cmd);
+        DataTable dt = new();
+        adapter.Fill(dt);
+        conn.Close();
+
+        string jsonStr = JsonConvert.SerializeObject(dt);
+        List<T> lst = JsonConvert.DeserializeObject<List<T>>(jsonStr)!;
+
+        return lst;
+    }
+
+    #endregion
+
+    #region Query First Or Default
+
+    public DataTable QueryFirstOrDefault(string query, SqlParameter[]? parameters)
+    {
+        SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
+        conn.Open();
+        SqlCommand cmd = new(query, conn);
+        cmd.Parameters.AddRange(parameters);
+        SqlDataAdapter adapter = new(cmd);
+        DataTable dt = new();
+        adapter.Fill(dt);
+        conn.Close();
+
+        return dt;
+    }
+
+    #endregion
+
+    #region Execute
+
+    public int Execute(string query, SqlParameter[]? parameters = null)
+    {
+        SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
+        conn.Open();
+        SqlCommand cmd = new(query, conn);
+        cmd.Parameters.AddRange(parameters);
+        int result = cmd.ExecuteNonQuery();
+        conn.Close();
+
+        return result;
+    }
+
+    #endregion
 }
