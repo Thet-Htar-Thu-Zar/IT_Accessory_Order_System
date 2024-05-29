1d936b7d98fa70bee8bb5779a419b30863a71281
Linn Thit <lth1212001@gmail.com>
2024-05-29 16:50:43 +0630

    -

--- Order_System/Models/User/RegisterRequestModel.cs
+++ Order_System/Models/User/RegisterRequestModel.cs
@@ -1,11 +1,10 @@
-﻿namespace Order_System.Models.User
+﻿namespace Order_System.Models.User;
+
+public class RegisterRequestModel
 {
-    public class RegisterRequestModel
-    {
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string PhoneNo { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNo { get; set; } = null!;
+    public string Password { get; set; } = null!;
 }
