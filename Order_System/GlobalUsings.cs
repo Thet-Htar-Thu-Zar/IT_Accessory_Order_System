bc2d9bfae44dea915873ba58f9d9573e91a95f5b
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-29 22:09:55 +0630

    -

---
+++ Order_System/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using System.Data.SqlClient;
+global using Microsoft.AspNetCore.Mvc;
+global using Order_System.Models.Cart;
+global using Order_System.Queries;
+global using Order_System.Service;
